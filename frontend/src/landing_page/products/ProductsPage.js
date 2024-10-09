c65d62a0c523a2779840ee5faed23af66b519c58
Geetansh431 <geetanshg2@gmail.com>
2024-10-10 02:49:09 +0530

    Added About and Support

--- frontend/src/landing_page/products/ProductsPage.js
+++ frontend/src/landing_page/products/ProductsPage.js
@@ -1,11 +1,25 @@
-import React from 'react'
-
+import React from "react";
+import Hero from "./Hero";
+import LeftSection from "./LeftSection";
+import RightSection from "./RightSection";
+import Universe from "./Universe";
 const ProductsPage = () => {
   return (
-    <div>
-      ProductsPage
-    </div>
-  )
-}
+    <>
+      <Hero />
+      <LeftSection
+        imageURL="media/images/kite.png"
+        productName="Kite"
+        productDescription="Our ultra-fast flagship trading platform with streaming market data, advanced charts, an elegant UI, and more. Enjoy the Kite experience seamlessly on your Android and iOS devices."
+        tryDemo=""
+        learnMore=""
+        googlePlay=""
+        appStore=""
+      />
+      <RightSection />
+      <Universe />
+    </>
+  );
+};
 
-export default ProductsPage
+export default ProductsPage;
